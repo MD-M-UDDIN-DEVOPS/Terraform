85eff92464c509ef6303d004a48d805839fd560f
Md M Uddin <86624642+MD-M-UDDIN-DEVOPS@users.noreply.github.com>
2023-03-24 08:37:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,13 @@
 provider "aws" {
 region = "us-east-1"
 }
+
+
 #Retrieve the list of AZs in the current AWS region
 data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
+
+
 #Define the VPC
 resource "aws_vpc" "vpc" {
 cidr_block = var.vpc_cidr
@@ -15,6 +19,8 @@ Environment = "demo_environment"
 Terraform = "true"
 }
 }
+
+
 #Deploy the private subnets
 resource "aws_subnet" "private_subnets" {
 for_each = var.private_subnets
@@ -27,6 +33,8 @@ Name = each.key
 Terraform = "true"
 }
 }
+
+
 #Deploy the public subnets
 resource "aws_subnet" "public_subnets" {
 for_each = var.public_subnets
@@ -40,12 +48,18 @@ Name = each.key
 Terraform = "true"
 }
 }
+
+
+
 #Create route tables for public and private subnets
 resource "aws_route_table" "public_route_table" {
 vpc_id = aws_vpc.vpc.id
 route {
 cidr_block = "0.0.0.0/0"
 gateway_id = aws_internet_gateway.internet_gateway.id
+        
+        
+        
 #nat_gateway_id = aws_nat_gateway.nat_gateway.id
 }
 tags = {
@@ -57,6 +71,9 @@ resource "aws_route_table" "private_route_table" {
 vpc_id = aws_vpc.vpc.id
 route {
 cidr_block = "0.0.0.0/0"
+        
+        
+        
 # gateway_id = aws_internet_gateway.internet_gateway.id
 nat_gateway_id = aws_nat_gateway.nat_gateway.id
 }
@@ -65,6 +82,9 @@ Name = "demo_private_rtb"
 Terraform = "true"
 }
 }
+
+
+
 #Create route table associations
 resource "aws_route_table_association" "public" {
 depends_on = [aws_subnet.public_subnets]
@@ -78,6 +98,9 @@ route_table_id = aws_route_table.private_route_table.id
 for_each = aws_subnet.private_subnets
 subnet_id = each.value.id
 }
+
+
+
 #Create Internet Gateway
 resource "aws_internet_gateway" "internet_gateway" {
 vpc_id = aws_vpc.vpc.id
@@ -93,6 +116,9 @@ tags = {
 Name = "demo_igw_eip"
 }
 }
+
+
+
 #Create NAT Gateway
 resource "aws_nat_gateway" "nat_gateway" {
 depends_on = [aws_subnet.public_subnets]
@@ -102,6 +128,8 @@ tags = {
 Name = "demo_nat_gateway"
 }
 }
+
+
 
 # Terraform Data Block - To Lookup Latest Ubuntu 20.04 AMI Image
 data "aws_ami" "ubuntu" {
@@ -116,6 +144,9 @@ values = ["hvm"]
 }
 owners = ["099720109477"]
 }
+
+
+
 # Terraform Resource Block - To Build EC2 instance in Public Subnet
 resource "aws_instance" "web_server" {
 ami = data.aws_ami.ubuntu.id
