ab41591fdb983c040859f184e6f64c2f1b9598ec
MD-M-UDDIN-DEVOPS <minhajuddin2008@gmail.com>
2023-03-26 09:41:58 -0400

    commit tf

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,3 @@
 terraform {
-    required_version = ">= 1.0.0"
-    required_providers {
-        aws = {
-            source = "harshicorp/aws"
-            version = "~> 3.0"
-
-        }
-    }
+  required_version = ">= 1.4.0"
 }
