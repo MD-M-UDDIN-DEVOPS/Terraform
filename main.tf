f3a0a0155e98957f4db1400ccd8c793c72376bd0
minhaj <minhajuddin2008@gmail.com>
2023-04-01 21:42:44 -0400

    Jenkins commit

--- main.tf
+++ main.tf
@@ -33,7 +33,21 @@ Name = each.key
 Terraform = "true"
 }
 }
+#Bootstrap Jenkins installation and start  
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo yum update -y
+  sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+  sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+  sudo yum upgrade
+  sudo amazon-linux-extras install java-openjdk11 -y
+  sudo yum install jenkins -y
+  sudo systemctl enable jenkins
+  sudo systemctl start jenkins
+  EOF
 
+  user_data_replace_on_change = true
+}
 
 #Deploy the public subnets
 resource "aws_subnet" "public_subnets" {
