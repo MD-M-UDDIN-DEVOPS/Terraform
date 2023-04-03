cd9ba40d00b80c8db4b0f844155f96d7cede0ceb
minhaj <minhajuddin2008@gmail.com>
2023-04-03 17:34:07 -0400

    update

--- provider/terraform.tf
+++ provider/terraform.tf
@@ -1,10 +1,25 @@
-#terraform {
-  required_version = ">= 1.4.0"
-  required_providers {
-    aws = {
-        source = "harshicorp/aws"
-        version = "~> 3.0"
-
-    }
-  }
+terraform {
+required_version = ">= 1.0.0"
+required_providers {
+aws = {
+source = "hashicorp/aws"
 }
+http = {
+source = "hashicorp/http"
+version = "2.1.0"
+}
+random = {
+source = "hashicorp/random"
+version = "3.1.0"
+}
+local = {
+source = "hashicorp/local"
+version = "2.1.0"
+}
+tls = {
+source = "hashicorp/tls"
+version = "3.1.0"
+}
+}
+}
+                                   
