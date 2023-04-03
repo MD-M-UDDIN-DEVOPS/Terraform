398800e46196bbe28c04aaae98c84115816ba082
minhaj <minhajuddin2008@gmail.com>
2023-04-02 22:28:49 -0400

    update

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ Terraform = "true"
   sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
   sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
   sudo yum upgrade
-  sudo amazon-linux-extras install java-openjdk11 -y
+  sudo amazon-linux-extras install java-openjdk11 -y (k)
   sudo yum install jenkins -y
   sudo systemctl enable jenkins
   sudo systemctl start jenkins
