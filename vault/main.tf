fca23cf902f1ffecc55992bffcdd0447b685a2e3
minhaj <minhajuddin2008@gmail.com>
2023-04-12 21:10:18 -0400

    commit

--- vault/main.tf
+++ vault/main.tf
@@ -140,3 +140,24 @@ Name = "sub-variables-us-east-1a"
 Terraform = "true"
 }
 }
+##########################################################
+module "autoscaling" {
+source = "terraform-aws-modules/autoscaling/aws"
+version = "4.9.0"
+# Autoscaling group
+name = "myasg"
+vpc_zone_identifier = [aws_subnet.private_subnets["private_subnet_1"].id
+, aws_subnet.private_subnets["private_subnet_2"].id, aws_subnet.
+private_subnets["private_subnet_3"].id]
+min_size = 0
+max_size = 1
+desired_capacity = 1
+# Launch template
+use_lt = true
+create_lt = true
+image_id = data.aws_ami.ubuntu.id
+instance_type = "t3.micro"
+tags_as_map = {
+Name = "Web EC2 Server 2"
+}
+}
